5466d42b9487bfb63912422d790474f51d31f378
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-09-22 21:12:55 -0500

    added Dashes

--- nick.js
+++ nick.js
@@ -1,4 +1,4 @@
 var nick = document.getElementById("nick"); // refers to the div tag with nick
 
 nick.innerHTML = "I like trees";// refers to spaces between div tag
-nick.innerHTML += '<br><img width="200px" src="style/tree.gif">';
+nick.innerHTML += '<br><img src="style/tree.gif">';
