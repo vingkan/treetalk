240e040aa673fc6fdc109410e51605cdd88eade0
Recursions <Nick@dhcp253.merusouth116.iit.edu>
2015-09-23 15:51:12 -0500

    small logics bug in nick.js
    Trying to append the treeList created in script.js to selector with id
    ‘selector-nick’

--- nick.js
+++ nick.js
@@ -9,8 +9,17 @@ function nickTree(){
 	alert(tree); // create a popup with string parameters
 }
 
-var treeList = document.getElementsByTagName("li");
+var treeList = document.getElementsByTagName("li"); // finds the list in script.js
 
 for (var i = 0; i < treeList.length; i++) {
 	console.log(treeList[i]);
 }
+
+// small error below
+var options = document.createElement('option'); // create a option tag element
+for (var i = 0; i < treeList.length; i++) { // loop through the treelist in script.js
+	options.appendChild(treeList[i]); // append each of the names in treeList
+	options.value += treeList[i]; // set the values of option to match the option
+	selectorNick.appendChild(options); // add options to selectorNick
+}
+
