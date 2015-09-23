a2a835e5276519c05ee463e68490379bd1c7dc36
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-09-22 21:10:17 -0500

    added gif

--- nick.js
+++ nick.js
@@ -1,3 +1,4 @@
 var nick = document.getElementById("nick"); // refers to the div tag with nick
 
 nick.innerHTML = "I like trees";// refers to spaces between div tag
+nick.innerHTML += '<br><img width="200px" src="style/tree.gif">';
