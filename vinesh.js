d2d371ff07a43b5abc2237f14615994c05e2e129
Vinesh Kannan <vingkan@gmail.com>
2015-09-22 21:09:30 -0500

    Added dank memes

--- vinesh.js
+++ vinesh.js
@@ -1,2 +1,3 @@
 var vinesh = document.getElementById('vinesh');
 	vinesh.innerHTML = "Nick is picking up JavaScript fast!";
+	vinesh.innerHTML += '<br><img width="200px" src="style/putinmemes.jpg">';
