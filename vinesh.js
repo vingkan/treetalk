b34b12e25a6e7d6b12a6454ef8a5f435c49668bc
Vinesh Kannan <vingkan@gmail.com>
2015-09-22 21:20:23 -0500

    Added simple form

--- vinesh.js
+++ vinesh.js
@@ -1,3 +1,10 @@
 var vinesh = document.getElementById('vinesh');
-	vinesh.innerHTML = "Nick is picking up JavaScript fast!";
-	vinesh.innerHTML += '<br><img src="style/putinmemes.jpg">';
+	/*vinesh.innerHTML = "Nick is picking up JavaScript fast!";
+	vinesh.innerHTML += '<br><img src="style/putinmemes.jpg">';*/
+
+var selectorVinesh = document.getElementById('selector-vinesh');
+
+function vineshTree(){
+	var tree = selectorVinesh.value;
+	alert(tree);
+}
