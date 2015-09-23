92df84ec2167b65fc8e59e6a133e24a0eb5857d9
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-09-22 21:20:33 -0500

    Buttons

--- nick.js
+++ nick.js
@@ -1,4 +1,10 @@
 var nick = document.getElementById("nick"); // refers to the div tag with nick
+var selectorNick = document.getElementById("selector-nick");
 
-nick.innerHTML = "I like trees";// refers to spaces between div tag
-nick.innerHTML += '<br><img src="style/tree.gif">';
+//nick.innerHTML = "I like trees";// refers to spaces between div tag
+//nick.innerHTML += '<br><img src="style/tree.gif">'; // injects html tags onto 
+
+function nickTree(){
+	var tree = selectorNick.value;
+	alert(tree); // create a popup with string parameters
+}
