3ce89a10b26a781d57e65debab1e0d9db98612f0
Vinesh Kannan <vingkan@gmail.com>
2015-09-29 20:02:52 -0500

    Made first Ajax call

--- socrata/calls.js
+++ socrata/calls.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+//https://data.illinois.gov/Municipality/Trees-Owned-by-the-City-of-Champaign/dzge-uybj
+
+var appToken = "Le00VXF0GK0d8D1tTn2v6Vkpl";
+
+$.ajax({
+	url: "https://data.chattlibrary.org/resource/5na4-ggsr.json",
+	method: "GET",
+	dataType: "json",
+	data: {
+		"codedescription": "EMBEZZLEMENT",
+		"$$app_token": appToken
+	},
+	success: function(data, status, jqxhr){
+		console.log("Received: ", data);
+	},
+	error: function(jqxhr, status, error){
+		console.log("Critical Error. RIP.");
+	}
+});
