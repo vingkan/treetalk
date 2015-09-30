be56eb797d990ba585e8d56414564bee23016dcc
Vinesh Kannan <vingkan@gmail.com>
2015-09-29 20:21:05 -0500

    Running call to Tree API

--- socrata/calls.js
+++ socrata/calls.js
@@ -4,18 +4,40 @@ console.log("Hello World");
 
 var appToken = "Le00VXF0GK0d8D1tTn2v6Vkpl";
 
-$.ajax({
-	url: "https://data.chattlibrary.org/resource/5na4-ggsr.json",
-	method: "GET",
-	dataType: "json",
-	data: {
-		"codedescription": "EMBEZZLEMENT",
-		"$$app_token": appToken
-	},
-	success: function(data, status, jqxhr){
-		console.log("Received: ", data);
-	},
-	error: function(jqxhr, status, error){
-		console.log("Critical Error. RIP.");
-	}
-});
+function sampleAjax(){
+	$.ajax({
+		url: "https://data.chattlibrary.org/resource/5na4-ggsr.json",
+		method: "GET",
+		dataType: "json",
+		data: {
+			"codedescription": "EMBEZZLEMENT",
+			"$$app_token": appToken
+		},
+		success: function(data, status, jqxhr){
+			console.log("Received: ", data);
+		},
+		error: function(jqxhr, status, error){
+			console.log("Critical Error. RIP.");
+		}
+	});
+}
+
+var treesAPIUrl = "https://data.illinois.gov/api/views/dzge-uybj/rows.json";
+
+function getTrees(){
+	$.ajax({
+		url: treesAPIUrl,
+		method: "GET",
+		dataType: "json",
+		data: {
+			"tree_species": "Acer rubrum",
+			"$$app_token": appToken
+		},
+		success: function(data, status, jqxhr){
+			console.log("Received: ", data);
+		},
+		error: function(jqxhr, status, error){
+			console.log("Critical Error. RIP.");
+		}
+	});
+}
