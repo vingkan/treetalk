ad6d75e20e664264f8b6b0c3455959eb7eb4b62e
Vinesh Kannan <vingkan@gmail.com>
2015-10-02 00:51:31 -0500

    Moved userlocation back

--- app/geolocation.js
+++ app/geolocation.js
@@ -1,3 +1,8 @@
+var userLocation = {
+	latitude: 0,
+	longitude: 0
+};
+
 function updateCoords(position){
 	userLocation.latitude = position.coords.latitude;
 	userLocation.longitude = position.coords.longitude;
