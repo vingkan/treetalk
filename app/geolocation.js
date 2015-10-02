d89604b017fdfe77361368c6738a148040bdeb94
Vinesh Kannan <vingkan@gmail.com>
2015-10-02 00:27:34 -0500

    Moved userLocation to main.js

--- app/geolocation.js
+++ app/geolocation.js
@@ -1,8 +1,3 @@
-var userLocation = {
-	latitude: 0,
-	longitude: 0
-};
-
 function updateCoords(position){
 	userLocation.latitude = position.coords.latitude;
 	userLocation.longitude = position.coords.longitude;
--- app/main.js
+++ app/main.js
@@ -3,6 +3,11 @@ var appToken = "Le00VXF0GK0d8D1tTn2v6Vkpl";
 
 var storage = new Storage();
 
+var userLocation = {
+	latitude: 0,
+	longitude: 0
+};
+
 navigator.geolocation.getCurrentPosition(updateCoords);
 
 getTrees('a1', {
