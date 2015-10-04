da221c24d5d32bea6e402a4957ce95d24047231c
Vinesh Kannan <vingkan@gmail.com>
2015-10-04 03:01:51 -0500

    Implemented random cloud generation

--- app/game/board.js
+++ app/game/board.js
@@ -3,6 +3,7 @@ Board.prototype.size = 400; //Pixels as Double
 Board.prototype.roadSize = 40; //Pixels as Double
 Board.prototype.roads = [] //Array of Roads
 Board.prototype.trees = [] //Array of Trees
+Board.prototype.clouds = [] //Array of Clouds
 
 function Board(gameSpaceID){
 	this.gameSpace = gameSpaceID || "gameSpace";
@@ -10,6 +11,7 @@ function Board(gameSpaceID){
 	this.roadSize = 40;
 	this.roads = [];
 	this.trees = [];
+	this.clouds = [];
 }
 
 Board.prototype.update = function(){
@@ -23,8 +25,14 @@ Board.prototype.toHTML = function(){
 	var xOffSet = this.roadSize;
 	var yOffSet = this.roadSize + (-1 * ((this.size - this.roadSize) / 2));
 	var offsetInterval = this.roadSize * 3;
+	for(var c = 0; c < 10000; c++){
+		var xRandom = Math.random() * ((this.size) + (yOffSet / 4));
+		var yRandom = Math.random() * ((this.size - this.roadSize));
+		html += '<div class="cloud" ';
+		html += 'style="margin-left:' + xRandom + 'px;';
+		html += 'margin-top:' + yRandom + 'px;"></div>';
+	}
 	for(var r = 0; r < this.roads.length; r++){
-		console.log('x: ' + xOffSet + ', y: ' + yOffSet);
 		if(this.roads[r].vertical){
 			html += this.roads[r].toHTML(yOffSet.toString() + 'px');
 			yOffSet += offsetInterval;
