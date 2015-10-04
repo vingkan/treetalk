5af4eebd9fb414de6bea9924027d7b8228719db3
Vinesh Kannan <vingkan@gmail.com>
2015-10-04 01:04:52 -0500

    Added Board.toHTML()

--- app/game/board.js
+++ app/game/board.js
@@ -1,7 +1,18 @@
+Board.prototype.gameSpace = "" //String: parent div for board
 Board.prototype.roads = [] //Array of Roads
 Board.prototype.trees = [] //Array of Trees
 
 function Board(){
+	this.gameSpace = "gameSpace";
 	this.roads = this.roads;
 	this.trees = this.trees;
+}
+
+Board.prototype.toHTML = function(){
+	html = '';
+	html += '<div class="board">';
+	for(var r = 0; r < this.roads.length; r++){
+		html += this.roads[r].toHTML();
+	}
+	html += '</div>';
 }
