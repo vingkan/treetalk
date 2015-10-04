69952fff7516736dd8482a74bfc9ac232d6f3bbc
Vinesh Kannan <vingkan@gmail.com>
2015-10-04 16:22:08 -0500

    Added cloudSpace dive

--- app/game/board.js
+++ app/game/board.js
@@ -16,12 +16,29 @@ function Board(id){
 
 Board.prototype.pollute = function(){
 	var pollution = 0;
-	pollution = 0 * this.roads.length;
+	pollution = 1 * this.roads.length;
 	this.clouds += pollution;
+}
+
+Board.prototype.printPollution = function(){
+	var html = "";
+	var xOffSet = this.roadSize;
+	var yOffSet = this.roadSize + (-1 * ((this.size - this.roadSize) / 2));
+	for(var c = 0; c < this.clouds; c++){
+		var xRandom = Math.random() * ((this.size) + (yOffSet / 4));
+		var yRandom = Math.random() * ((this.size - this.roadSize));
+		html += '<div class="cloud" ';
+		html += 'style="margin-left:' + xRandom + 'px;';
+		html += 'margin-top:' + yRandom + 'px;"></div>';
+	}
+	if(printed){
+		document.getElementById(this.id + '-clouds').innerHTML = html;
+	}
 }
 
 Board.prototype.update = function(){
 	this.pollute();
+	this.printPollution();
 	for(var r = 0; r < this.roads.length; r++){
 		this.roads[r].update(this.size, this.roadSize);
 	}
@@ -40,13 +57,8 @@ Board.prototype.toHTML = function(){
 	var xOffSet = this.roadSize;
 	var yOffSet = this.roadSize + (-1 * ((this.size - this.roadSize) / 2));
 	var offsetInterval = this.roadSize * 3;
-	for(var c = 0; c < this.clouds; c++){
-		var xRandom = Math.random() * ((this.size) + (yOffSet / 4));
-		var yRandom = Math.random() * ((this.size - this.roadSize));
-		html += '<div class="cloud" ';
-		html += 'style="margin-left:' + xRandom + 'px;';
-		html += 'margin-top:' + yRandom + 'px;"></div>';
-	}
+	html += '<div id="' + this.id + '-clouds" class="cloudSpace">';
+	html += '</div>';
 	for(var r = 0; r < this.roads.length; r++){
 		if(this.roads[r].vertical){
 			html += this.roads[r].toHTML(yOffSet.toString() + 'px');
