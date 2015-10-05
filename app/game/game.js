1bbf27bf4f98b2fcd4a557e2e59a68ef31b52233
Vinesh Kannan <vingkan@gmail.com>
2015-10-05 17:38:37 -0500

    Started the instructions sequence

--- app/game/game.js
+++ app/game/game.js
@@ -9,9 +9,21 @@ function getWindowDimensionLimit(){
 }
 
 function updateGameWindow(){
-	var limitSize = getWindowDimensionLimit() * 0.75;
+	var limitSize = getWindowDimensionLimit() * 0.8;
 	board.size = limitSize;
 	board.roadSize = limitSize / 12;
 	updateBoardSize(limitSize.toString() + "px");
 	board.print();
+}
+
+function instructionsAlert(message, time){
+	document.getElementById('instructions').innerHTML = message;
+	document.getElementById('instructionsAlert').style.height = '10vh';
+	setTimeout(function(){
+		document.getElementById('instructionsAlert').style.height = '0vh';
+	}, time);
+}
+
+function instructionsSequence(){
+	instructionsAlert("Welcome to Tree Talk: The Game!", 1000);
 }
